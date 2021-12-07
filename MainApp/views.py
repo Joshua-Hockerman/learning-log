7237370391aadc8f35e8d6e274de7cd18611ed98
Joshua Hockerman <jhockerman96@gmail.com>
2021-12-06 22:38:44 -0600

    Commit to add the user functionality and login requirements/locking.

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,20 +1,24 @@
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from .forms import TopicForm, EntryForm
 from .models import Topic, Entry
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request, "MainApp/index.html")
 
 
+@login_required  # this is a decorator, that requires it to be done before the action can be taken
 def topics(request):
-    topics = Topic.objects.all().order_by("date_added")
+    topics = Topic.objects.filter(owner=request.user).order_by("date_added")
 
     context = {"topics": topics}
 
     return render(request, "MainApp/topics.html", context)
 
 
+@login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by("-date_added")
@@ -23,6 +27,7 @@ def topic(request, topic_id):
     return render(request, "MainApp/topic.html", context)
 
 
+@login_required
 def new_topic(request):
     if request.method != "POST":
         form = TopicForm()
@@ -38,6 +43,7 @@ def new_topic(request):
     return render(request, "MainApp/new_topic.html", context)
 
 
+@login_required
 def new_entry(request, topic_id):
 
     topic = Topic.objects.get(id=topic_id)
@@ -61,9 +67,10 @@ def new_entry(request, topic_id):
     return render(request, "MainApp/new_entry.html", context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
-    topic = Entry.topic
+    topic = entry.topic
 
     if request.method != "POST":
         # This argument tells Django to create the form prefilled
